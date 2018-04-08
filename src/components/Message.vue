e9dffdc90d4622f3c0c2ed44b7d848495f8e87ab
scalaview <chailink100@gmail.com>
2018-04-09 00:34:21 +0800

    input transaction

---
+++ src/components/Message.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="notification" v-bind:class="{'is-danger': error, 'is-info': !error}" v-if="message">
+    <button class="delete" v-on:click="clearMessage"></button>
+    {{ message }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'message',
+  props: {
+    message: {
+      type: String,
+      default () {
+        return ''
+      }
+    },
+    error: {
+      type: Boolean,
+      default () {
+        return false
+      }
+    }
+  },
+  methods: {
+    clearMessage () {
+      this.$emit('update:msg', '')
+    }
+  }
+}
+</script>
--- src/views/Wallet.vue
+++ src/views/Wallet.vue
@@ -22,8 +22,44 @@
       </div>
     </div>
 
-    <section class="hero is-primary">
-      <a class="button is-large">Send Transaction</a>
+    <section class="section">
+      <div class="panel-block" v-if="msg">
+        <div class="container">
+          <message v-bind:message="msg" v-bind:error="error" v-on:update:msg="val => msg = val"></message>
+        </div>
+      </div>
+      <div>
+        <div class="field">
+          <label class="label">Address</label>
+          <div class="control has-icons-left has-icons-right">
+            <input class="input is-success" type="text" placeholder="Address" v-model="toaddress">
+          </div>
+          <p class="help is-success">This username is available</p>
+        </div>
+
+        <div class="field has-addons">
+          <p class="control has-icons-left">
+            <input class="input" type="number" v-on:change.prevent.self="validatAmount" v-bind:class="{'is-danger': (inValidatAmount)}" v-model="amount" placeholder="Amount of money">
+          </p>
+          <p class="control">
+            <a class="button">
+              {{ balance }}
+            </a>
+          </p>
+        </div>
+        <p class="help" v-if="inValidatAmount" v-bind:class="{'is-danger': (inValidatAmount)}" >Amount invalidat</p>
+
+
+        <div class="field is-grouped">
+          <div class="control">
+            <button class="button is-primary" v-bind:class="{'is-loading': isLoading }" v-on:click="sendTransaction">Submit</button>
+          </div>
+          <div class="control">
+            <button class="button is-text" v-on:click="reset">Reset</button>
+          </div>
+        </div>
+
+      </div>
     </section>
 
   </section>
@@ -32,30 +68,56 @@
 </template>
 
 <script>
+import Message from '@/components/Message'
 
 export default {
   name: 'wallet',
   components: {
+    Message
   },
   data () {
     return {
+      msg: '',
+      error: false,
       empty: true,
       address: '',
       privkey: '',
-      balance: 0.00
+      balance: 0.00,
+      amount: 0.00,
+      toaddress: "",
+      isLoading: false,
+      inValidatAmount: true
     }
   },
   computed: {
     isEmpty(){
       return this.empty
-    }
+    },
+
   },
   methods: {
     generate(){
-      console.log("click")
+      this.isLoading = true
+      this.error = false
+      this.msg = 'Wallet create successfully!'
       this.empty = false
       this.address = "dwfwagtgabfhijrjtgbfhawjjvfujhvxjhearbjgaohwfncbsehio"
       this.privkey = 'dwefwagvavurijkjghuiarjhbjfrugihkjwergsdbfhbejfwnersvfbhiu'
+      this.isLoading = false
+    },
+    reset(){
+      this.toaddress = ""
+      this.amount = 0.00
+    },
+    sendTransaction(){
+      this.isLoading = true
+    },
+    validatAmount(){
+      if(this.amount > this.balance){
+        this.inValidatAmount = true
+      }else{
+        this.inValidatAmount = false
+      }
     }
   }
 }
